4a7584b24c04f6b0ba4b2d116e710e290194882a
Michael Rivera <37431175+lmriii@users.noreply.github.com>
2022-04-07 21:00:39 -0700

    initial commit

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,26 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const articles = require('./controllers/routes/articles');
+
+
+// middleware
+
+app.use(express.json());
+
+
+// routes
+
+app.use('/api/v1/articles', articles);
+
+
+app.get('/hello', (req, res, next) => {
+    res.send('Task Manager and App');
+})
+
+const port = 4000;
+
+app.listen(port, console.log(`Server is listening on port ${port}...`));
+
+
+
+
