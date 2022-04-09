380171bd0e89422b9a6eef9bc4c7f3db5dd9a7e9
Michael Rivera <37431175+lmriii@users.noreply.github.com>
2022-04-09 10:33:21 -0700

    setting up .env variables

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const articles = require('./controllers/routes/articles');
 const connectDB = require('./db/connect');
+require('dotenv').config();
 
 
 // middleware
@@ -24,10 +25,10 @@ const port = 4000;
 
 const start = async () => {
     try {
-        await connectDB()
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening on port ${port}...`));
     } catch (error) {
-        
+        console.log(error);
     }
 }
 
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 
-const connectionString = 'mongodb+srv://admin:UVasevL7NjTpiXys@twc-backend.8go77.mongodb.net/blog-articles?retryWrites=true&w=majority';
+
 
 const connectDB = (url) => {
     return mongoose
-    .connect(connectionString,{ 
+    .connect(url,{ 
         useNewUrlParser: true,
         useUnifiedTopology: true})
 };
